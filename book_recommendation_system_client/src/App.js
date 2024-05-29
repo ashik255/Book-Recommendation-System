3bf1a1006009b134dfacde1185443564a4b70160
ashik255 <ariful15-11556@diu.edu.bd>
2024-05-30 01:11:12 +0600

    NavBar Create

--- book_recommendation_system_client/src/App.js
+++ book_recommendation_system_client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import NavBar from './shared/NavBar/NavBar.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1> hii</h1>
+      <NavBar />
+
     </div>
   );
 }
